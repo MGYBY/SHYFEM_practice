d243c80ebf8863fcaa926c0e83b29ce24a3970fc
BYY <53412286+MGYBY@users.noreply.github.com>
2025-02-09 22:23:36 -0500

    Add files via upload

---
+++ shyfem_profile.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+#------------------------------------------------------------------------
+#
+#    Copyright (C) 1985-2020  Georg Umgiesser
+#
+#    This file is part of SHYFEM.
+#
+#------------------------------------------------------------------------
+#
+# shyfem_profile.sh
+#
+# sets path and aliases
+#
+# is called from within .bashrc, .bash_profile, .profile
+#
+#------------------------------------------------------
+
+dist=shyfem
+
+FEMDIR=${SHYFEMDIR:=$HOME/$dist}
+#fembin=$FEMDIR/bin
+fembin=$FEMDIR/fembin
+
+FEMDIR_INSTALL=${SHYFEM_INSTALL:=$HOME/$dist}
+#fembin_install=$FEMDIR_INSTALL/bin
+fembin_install=$FEMDIR_INSTALL/fembin
+
+# set PATH ----------------------------------------
+
+binutil=$fembin_install
+[ -d $fembin_install/shyfem_util ] && binutil=$fembin_install/shyfem_util
+
+path=`$binutil/shyfem_path.pl $PATH`
+export PATH=$path:$fembin:$HOME/$dist/bin
+
+# set aliases ----------------------------------------
+
+alias shyfemdir=". $binutil/shyfem_dir.sh"
+alias shyfeminstall=". $binutil/shyfem_dir.sh $FEMDIR_INSTALL"
+alias shyfemcd="cd $FEMDIR"
+
+# end of routine ----------------------------------------
+
